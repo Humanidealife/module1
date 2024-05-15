8ce4cb29089631ed0822c19c524ae4ba3c8f760e
WANG <wangqianlf@live.com>
2024-05-15 22:23:22 +0200

    modif de la Class Bonjour

--- src/main/java/com/mycompany/testlibrairiepremier/module1/Bonjour.java
+++ src/main/java/com/mycompany/testlibrairiepremier/module1/Bonjour.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -12,7 +12,7 @@ public class Bonjour {
     //On ajoute une méthode
     public void printGreetings (String nom, String prenom){
         System.out.println("Bonjour " + prenom + " " + nom);
-        //On clique à nouveau "Clean and Build" sur module1, 
+        //On clique à nouveau "Clean and Build" sur "module1" dans l'onglet à gauche de l'écran , 
         //  pour fabriquer à nouveau notre librairie (donc pour raffraîchir la librairie).
         //Le "Clean ans Build" a eu pour effet de rafraîchir notre librairie "module1-1.jar" dans le dossier "target", 
         //  mais également dans le dossier ".m2"
